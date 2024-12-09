13f970d70e01b34b45a054d26a862910dbc223e7
PriyadharshiniMuthukumar <priyadharshini.muthukumar@siemens.com>
2024-12-09 11:11:47 +0530

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Namespace
+{
+    public class ProductController : Controller
+    {
+        // GET: ProductController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+int num1 = 10;
+int num2 = 4;
+int add = num1 + num2;
+Console.WriteLine("Final Value:"+ add);
